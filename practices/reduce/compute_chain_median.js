bbe0f9df8ca3956e8dfa5f6123fd955e0899f7c1
zhangs2 <sha.zhang@dianrong.com>
2018-02-11 18:26:32 +0800

    update code

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,14 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  let sortNumberArray = collection.split('->').sort((a, b) => a - b);
+
+  if (sortNumberArray.length / 2 !== 0) {
+    return sortNumberArray[sortNumberArray.length / 2];
+  } else {
+    return
+  }
+  return parseFloat((sortNumberArray[sortNumberArray.length / 2 - 1] + sortNumberArray[sortNumberArray.length / 2]) / 2);
 }
 
 module.exports = compute_chain_median;
